881f85d969d76afb55e5ca42371c5bfc6578bcfc
Soorya <soorisoorya666@gmail.com>
2026-03-14 22:54:28 +0530

    Initialize App component with portfolio layout

---
+++ app.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App(){
+
+return(
+
+<div className="main">
+
+<h1>OrbitUI Portfolio Interface</h1>
+
+<div className="orbit">
+
+<div className="planet">Projects</div>
+<div className="planet">Skills</div>
+<div className="planet">Experience</div>
+
+</div>
+
+</div>
+
+)
+
+}
+
+export default App;
